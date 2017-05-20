bfa93d25a801b5debacbbdbccfb5d22c654cb818
vagrant <vagrant@homestead>
2017-05-20 21:20:17 +0000

    front acabado

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     <title>Natura</title>
 </head>
 <body>
-
+    <div class="content">
+        <form action="">
+            <section>
+                <div class="primero"><input type="text" name="nombre" placeholder="Nombre y Apellido"></div>
+                <div class="segundo"><input type="text" style="    margin-right: 11px;" name="correo" placeholder="Correo"><input name="celular" placeholder="Celular" type="text"></div>
+            </section>
+            <button><img src="img/boton.png" alt=""></button>
+        </form>
+    </div>
 </body>
 </html>
