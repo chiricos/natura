6098711c04cc9897b8653b3e1da249f1575177dc
chiricos <edwarddiaz92@gmail.com>
2017-05-20 15:02:24 -0500

    subiendo cambios

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html >
+<head>
+    <link rel="stylesheet" type="text/css" href="css/normalice.css" >
+    <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <script src="js/script.js"></script>
+    <title>Natura</title>
+</head>
+<body>
+
+</body>
+</html>
